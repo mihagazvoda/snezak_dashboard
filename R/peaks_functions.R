e77db82b62ad251fa4df9f0e4da47faf621a2ca8
mihagazvoda <miha.gazvoda1@gmail.com>
2020-04-07 08:16:00 +0200

    change handling logic

--- R/peaks_functions.R
+++ R/peaks_functions.R
@@ -1,30 +1,40 @@
 get_peaks <- function(path_peaks) {
-  ifelse(
-    file.exists(path_peaks),
-    readr::read_rds(path_peaks), 
-    get_peaks_from_osm() %>% save_rds(path_peaks)
-  ) 
+  if (file.exists(path_peaks)) {
+    readr::read_rds(path_peaks)
+  } else {
+    {
+      get_osm_points() %>% save_rds(path_peaks)
+    } %>%
+      get_peaks_from_osm_points()
+  }
 }
 
-save_rds <- function(file, path){
-  readr::write_rds(deduplicated_peaks, path = path)
-  file
-}
-
-get_peaks_from_osm <- function() {
-  peak_osm_points <- opq(bbox = c(11, 45.5, 16, 48), ) %>% # 
+get_osm_points <- function() {
+  opq(bbox = c(13.47, 46.11, 14.30, 46.54)) %>% # c(11, 45.5, 16, 48)
     add_osm_feature(key = "natural", value = "peak") %>%
     osmdata_sf() %>%
-    purrr::pluck("osm_points")
-  
-  tidy_peaks <- peak_osm_points %>%
-    filter(!is.na(name)) %>%
-    select(name, ele, geometry, wikipedia, prominence, importance, massif, wikidata) %>%
-    `[<-`(, c("lat", "lon"), map(transpose(.$geometry), unlist))
-  
-  deduplicated_peaks <- tidy_peaks %>%
+    purrr::pluck("osm_points") %>%
+    mutate(
+      lon = st_coordinates(geometry)[, 1],
+      lat = st_coordinates(geometry)[, 2]
+    ) 
+  # %>% tibble()
+}
+
+get_peaks_from_osm_points <- function(peak_osm_points) {
+  peak_osm_points %>%
+    filter(!is.na(name) & ele > 1000) %>%
     group_by(name) %>%
-    arrange(desc(prominence), importance, wikipedia, wikidata) %>%
-    filter(row_number() == 1) %>% 
+    arrange(desc(prominence), wikipedia, wikidata) %>%
+    filter(row_number() == 1) %>%
+    mutate(
+      lon = st_coordinates(geometry)[, 1],
+      lat = st_coordinates(geometry)[, 2]
+    ) %>%
     select(name, lat, lon)
+}
+
+save_rds <- function(file, path) {
+  readr::write_rds(file, path = path)
+  invisible(file)
 }
