bfa2f41f40f6d53903f82a09acb17688d2a728cc
mihagazvoda <miha.gazvoda1@gmail.com>
2020-04-09 21:02:19 +0200

    join peaks and ski_tours

--- R/plan.R
+++ R/plan.R
@@ -1,4 +1,5 @@
 plan <- drake_plan(
   ski_tours = extract_all_ski_tours(),
-  peaks = get_peaks("./data/osm_points.rds")
+  peaks = get_peaks("./data/osm_points.rds"), 
+  df = left_join(ski_tours, rename(peaks, peak = 'name'), by = 'peak')
 )
