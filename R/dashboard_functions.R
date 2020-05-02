731858523f49d18cce1dd484d94ac1fba1991e9b
mihagazvoda <miha.gazvoda1@gmail.com>
2020-05-02 13:18:49 +0200

    add deploy step

--- R/dashboard_functions.R
+++ R/dashboard_functions.R
@@ -1,7 +1,23 @@
-deploy_app <- function(file) {
-  rsconnect::deployApp(
+deploy_app <- function(file, ...) {
+  deployApp(
     appFiles = file,
     appName = "snezak",
     forceUpdate = TRUE
   )
 }
+
+pal <- colorNumeric(
+  palette = "RdBu",
+  domain = c(1, 5)
+)
+
+peak_url <- function(peak) {
+  paste0(
+    "https://www.snezak.si/search.html?key=",
+    stringr::str_replace_all(peak, pattern = " ", replacement = "+")
+  )
+}
+
+specify_decimal <- function(x, k) {
+  format(round(x, k), nsmall=k)
+} 
